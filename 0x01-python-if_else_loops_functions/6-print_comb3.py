ec17d33e05f728b505153cf73b637e0461fbfe1d
Felix Adeh Essienne <nanaitconsult@gmail.com>
2022-08-23 21:12:55 -0700

    comb3

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 for number in range(0,90):
-    if number / 10 < num % 10:
+    if number / 10 < number % 10:
         if number != 89:
-            print('{:02d}'.format(number), end=", ")
+            print('{:02d}'.format(number), end=",")
         else:
-            print('{:02d}'.format(num+1))
+            print('{:02d}'.format(number+1))
