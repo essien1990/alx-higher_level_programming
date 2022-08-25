6a6ee88be54ef2334013463655994bec8f63782f
Felix Adeh Essienne <nanaitconsult@gmail.com>
2022-08-25 13:25:08 -0700

    calc

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -7,10 +7,10 @@ if __name__ == "__main__":
         print("Usage: ./100-my_calculator.py <a> <operator> <b>")
         sys.exit(1)
 
-     opr = sys.argv[2]
-     if opr != '+' and opr != '-' and opr != '*' and opr != '/':
-         print("Unknown operator. Available operators: +, -, * and /")
-         sys.exit(1)
+    opr = sys.argv[2]
+    if opr != '+' and opr != '-' and opr != '*' and opr != '/':
+        print("Unknown operator. Available operators: +, -, * and /")
+        sys.exit(1)
 
     from calculator_1 import add, sub, mul, div
     a = int(sys.argv[1])
