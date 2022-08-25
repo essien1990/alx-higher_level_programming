dfcc59f6e38c1780b58cff8af8bb238cc1d1158a
Felix Adeh Essienne <nanaitconsult@gmail.com>
2022-08-25 12:51:49 -0700

    calculator

---
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+    import sys
+
+    iargs = len(sys.argv) - 1
+    if iargs != 3:
+        print("Usage: ./100-my_calculator.py <a> <operator> <b>")
+        sys.exit(1)
+
+    opr = sys.argv[2]
+    if opr != '+' and opr != '-' and opr != '*' and opr != '/':
+        print("Unknown operator. Available operators: +, -, * and / ")
+        sys.exit(1)
+
+    from calculator_1 import add, sub, mul, div
+    a = sys.argv[1]
+    b = sys.argv[3]
+
+    if opr == '+':
+        print("{} + {} = {}".format(a, b, add(a, b)))
+    elif opr == '-':
+        print("{} - {} = {}".format(a, b, sub(a, b)))
+    elif opr == '*':
+        print("{} * {} = {}".format(a, b, mul(a, b)))
+    else:
+        print("{} / {} = {}".format(a, b, div(a, b)))
