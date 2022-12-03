b880123846696de0f201dc1cba111ddc2b3830f5
Felix Adeh Essienne <nanaitconsult@gmail.com>
2022-12-03 18:35:30 +0000

    update_safe_filter_states

--- 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -1,10 +1,34 @@
-#!/usr/bin/python3
+if __name__ == '__main__':
+    from sys import argv
+    import MySQLdb as mysql
+    import re
 
-import sys
-import MySQLdb
+    if (len(argv) != 5):
+        print('Use: username, password, database name, state name')
+        exit(1)
 
-if __name__ == "__main__":
-    db = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
-    c = db.cursor()
-    c.execute("""SELECT * FROM states""")
-    [print(state) for state in c.fetchall() if state[1] == sys.argv[4]]
+    searched = ' '.join(argv[4].split())
+
+    if (re.search('^[a-zA-Z ]+$', searched) is None):
+        print('Enter a valid name state (example: Arizona)')
+        exit(1)
+
+    try:
+        db = mysql.connect(host='localhost', port=3306, user=argv[1],
+                           passwd=argv[2], db=argv[3])
+    except Exception:
+        print('Failed to connect to the database')
+        exit(0)
+
+    cursor = db.cursor()
+
+    cursor.execute("SELECT * FROM states \
+                    WHERE name = '{:s}' ORDER BY id ASC;".format(searched))
+
+    result_query = cursor.fetchall()
+
+    for row in result_query:
+        print(row)
+
+    cursor.close()
+    db.close()
