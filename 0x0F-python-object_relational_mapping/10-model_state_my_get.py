5df780148b85e021c966a700232a799f462e0244
Felix Adeh Essienne <nanaitconsult@gmail.com>
2022-12-03 17:35:49 +0000

    update_model_state

--- 0x0F-python-object_relational_mapping/10-model_state_my_get.py
+++ 0x0F-python-object_relational_mapping/10-model_state_my_get.py
@@ -1,3 +1,6 @@
+#!/usr/bin/python3
+""" prints the State object with the name passed as argument from the database """
+
 import sqlalchemy
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
