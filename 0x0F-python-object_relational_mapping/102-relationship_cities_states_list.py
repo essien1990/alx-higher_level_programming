cd542860fcf4eba9483938cd6ef010467cd6fb5f
Felix Adeh Essienne <nanaitconsult@gmail.com>
2022-12-03 18:23:18 +0000

    update_relationship_states_cities

--- 0x0F-python-object_relational_mapping/102-relationship_cities_states_list.py
+++ 0x0F-python-object_relational_mapping/102-relationship_cities_states_list.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
-
+"""
+Lists all City objects from the database hbtn_0e_101_usa
+"""
 import sys
 from relationship_state import Base, State
 from relationship_city import City
@@ -20,4 +22,3 @@ if __name__ == '__main__':
     for state in st:
         for city in state.cities:
             print("{}: {} -> {}".format(city.id, city.name, state.name))
-    session.close()
